5f45342ba1cfedb982b81559dc4a75892394a20c
Ethan Wessel <ejwessel@gmail.com>
2019-12-08 16:18:32 -0500

    solution for part 2 day 4

--- Day_4/SecureContainer.py
+++ Day_4/SecureContainer.py
@@ -10,6 +10,7 @@ def check_increasing(num_str):
         min_str = num_str[i]
     return valid
 
+
 def check_duplicates(num_str):
     duplicates = False
     for i in range(0, len(num_str) - 1):
@@ -17,6 +18,26 @@ def check_duplicates(num_str):
             duplicates = True
             break
     return duplicates
+
+
+def check_duplicates_freq(num_str):
+    num_freq = {}
+    for i in range(0, len(num_str)):
+        if num_str[i] not in num_freq:
+            num_freq[num_str[i]] = 1
+        else:
+            num_freq[num_str[i]] += 1
+
+    for num in num_freq.values():
+        # skip if freq is 1
+        if num == 1:
+            continue
+        # # skip if freq is even, meaning it's not of 2 pair
+        # if num % 2 == 0:
+        #     return True
+        if num == 2:
+            return True
+    return False
 
 
 if __name__ == "__main__":
@@ -34,3 +55,32 @@ if __name__ == "__main__":
     # Check the answer
     print(valid_count)
     assert valid_count == 1694
+
+    # Part 2 ================
+
+    result = check_duplicates_freq("156666")
+    assert result == False
+    result = check_duplicates_freq("123444")
+    assert result == False
+    result = check_duplicates_freq("111122")
+    assert result == True
+    reult = check_duplicates_freq("112233")
+    assert result == True
+    result = check_duplicates_freq("122244")
+    assert result == True
+
+    valid_count = 0
+    for num in range(156218, 652527):
+        num_str = str(num)
+        if not check_increasing(num_str):
+            continue
+        if not check_duplicates(num_str):
+            continue
+        if not check_duplicates_freq(num_str):
+            continue
+        # We made it here so the number must be valid
+        valid_count += 1
+
+    # Check the answer
+    print(valid_count)
+    assert valid_count == 1148
