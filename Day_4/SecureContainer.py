0e6b61f27c57a30c34a421f396ec8cc13ae24832
Ethan Wessel <ejwessel@gmail.com>
2019-12-08 14:59:25 -0500

    clean code

--- Day_4/SecureContainer.py
+++ Day_4/SecureContainer.py
@@ -1,30 +1,36 @@
-if __name__ == "__main__":
 
+def check_increasing(num_str):
+    valid = True
+    min_str = num_str[0]
+    for i in range(1, len(num_str)):
+        # if encountering a num that is less than the seen min then stop
+        if int(num_str[i]) < int(min_str):
+            valid = False
+            break
+        min_str = num_str[i]
+    return valid
+
+def check_duplicates(num_str):
+    duplicates = False
+    for i in range(0, len(num_str) - 1):
+        if num_str[i] == num_str[i + 1]:
+            duplicates = True
+            break
+    return duplicates
+
+
+if __name__ == "__main__":
     valid_count = 0
     for num in range(156218, 652527):
-
-        # Rule - Increasing Check
         num_str = str(num)
-        valid = True
-        min_str = num_str[0]
-        for i in range(1, len(num_str)):
-            # if encountering a num that is less than the seen min then stop
-            if int(num_str[i]) < int(min_str):
-                valid = False
-                break
-            min_str = num_str[i]
+        if not check_increasing(num_str):
+            continue
+        if not check_duplicates(num_str):
+            continue
 
-        # Rule - Duplicate
-        duplicates = False
-        if valid:
-            for i in range(0, len(num_str) - 1):
-                if num_str[i] == num_str[i + 1]:
-                    duplicates = True
-                    break
+        # We made it here so the number must be valid
+        valid_count += 1
 
-        # check if rules hold
-        if valid and duplicates:
-            print(num)
-            valid_count += 1
-
+    # Check the answer
     print(valid_count)
+    assert valid_count == 1694
