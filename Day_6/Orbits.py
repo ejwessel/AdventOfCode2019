1294219ed75e65fc893cd4482560f84500c6a7d6
Ethan Wessel <ethan.wessel@fluidity.io>
2019-12-10 16:16:38 -0500

    Day 6 Setup

---
+++ Day_6/Orbits.py
@@ -0,0 +1,14 @@
+
+
+if __name__ == "__main__":
+    sum = 0
+    for num in range(3):
+        sum += (num + 1)
+
+    for num in range(4):
+        sum += (num + 1)
+
+    for num in range(7):
+        sum += (num + 1)
+
+    print(sum)
