b6dc889eef332169102926d1f699528e7f974983
Ethan Wessel <ejwessel@gmail.com>
2019-12-12 22:15:32 -0500

    queing mechanism

--- Day_7/IntcodeProgram.py
+++ Day_7/IntcodeProgram.py
@@ -18,38 +18,9 @@ class IntcodeProgram:
         POSITION = 0
         IMMEDIATE = 1
 
-    def run_intcode_max_signal(self, phase_settings, start_signal, file_input):
-        '''
-        To do this, before running the program,
-        replace position 1 with the value 12 and
-        replace position 2 with the value 2.
-        What value is left at position 0 after the program halts?
-        '''
-
-        with open(file_input) as data:
-            for line in data:
-                # list comprehension to turn all strings in list to ints
-                input_values = [int(str_num) for str_num in line.split(',')]
-
-                return self.run_max_signal(phase_settings, input_values, start_signal)
-
-    def run_max_signal(self, phase_settings, input_values, start_signal):
-        permuted_phases = permutations(phase_settings)
-        max_signal = 0
-        for phases in permuted_phases:
-            signal = start_signal
-
-            for phase in phases:
-
-                # for a given permutation I need to keep feeding it into the same thing
-
-                output = self.run(input_values, [phase, signal])
-                signal = output[0]
-                input_values = output[1:]
-
-            if signal > max_signal:
-                max_signal = signal
-        return max_signal
+    def __init__(self, program):
+        self.instruction_pointer = 0
+        self.program = program.copy()
 
     def get_instruction_set(self, instruction):
         # depending on the instruction we will parse the digits in some fashion
@@ -181,69 +152,241 @@ class IntcodeProgram:
         else:
             input_codes[save_idx] = 0
 
-    def run(self, input_codes, input_signal):
-        instruction_pointer = 0
+    def run(self, input_signal):
         while True:
-            instruction = input_codes[instruction_pointer]
+            instruction = self.program[self.instruction_pointer]
             instruction_set = self.get_instruction_set(instruction)
             opcode = instruction_set[0]
 
             if opcode == self.Opcodes.HALT.value:
                 break
             elif opcode == self.Opcodes.ADD.value:
-                self.add(input_codes, instruction_set, instruction_pointer)
-                instruction_pointer += 4
+                self.add(self.program, instruction_set, self.instruction_pointer)
+                self.instruction_pointer += 4
             elif opcode == self.Opcodes.MUL.value:
-                self.mul(input_codes, instruction_set, instruction_pointer)
-                instruction_pointer += 4
+                self.mul(self.program, instruction_set, self.instruction_pointer)
+                self.instruction_pointer += 4
             elif opcode == self.Opcodes.SAVE.value:
-                self.save(input_codes, input_signal[0], instruction_set, instruction_pointer)
+                if len(input_signal) == 0:
+                    break
+                self.save(self.program, input_signal[0], instruction_set, self.instruction_pointer)
+                self.instruction_pointer += 2
                 input_signal = input_signal[1:]
-                instruction_pointer += 2
             elif opcode == self.Opcodes.READ.value:
-                return [self.output(input_codes, instruction_set, instruction_pointer)] + input_codes
+                result = self.output(self.program, instruction_set, self.instruction_pointer)
+                self.instruction_pointer += 2
+                return result
             elif opcode == self.Opcodes.JUMP_T.value:
-                new_pointer = self.jump_t(input_codes, instruction_set, instruction_pointer)
-                instruction_pointer = new_pointer if new_pointer is not None else instruction_pointer + 3
+                new_pointer = self.jump_t(self.program, instruction_set, self.instruction_pointer)
+                self.instruction_pointer = new_pointer if new_pointer is not None else self.instruction_pointer + 3
             elif opcode == self.Opcodes.JUMP_F.value:
-                new_pointer = self.jump_f(input_codes, instruction_set, instruction_pointer)
-                instruction_pointer = new_pointer if new_pointer is not None else instruction_pointer + 3
+                new_pointer = self.jump_f(self.program, instruction_set, self.instruction_pointer)
+                self.instruction_pointer = new_pointer if new_pointer is not None else self.instruction_pointer + 3
             elif opcode == self.Opcodes.LESS_THAN.value:
-                self.less_than(input_codes, instruction_set, instruction_pointer)
-                instruction_pointer += 4
+                self.less_than(self.program, instruction_set, self.instruction_pointer)
+                self.instruction_pointer += 4
             elif opcode == self.Opcodes.EQUAL.value:
-                self.equal(input_codes, instruction_set, instruction_pointer)
-                instruction_pointer += 4
+                self.equal(self.program, instruction_set, self.instruction_pointer)
+                self.instruction_pointer += 4
             else:
                 break
 
+
+def run_intcode_max_signal(self, phase_settings, start_signal, file_input):
+    '''
+    To do this, before running the program,
+    replace position 1 with the value 12 and
+    replace position 2 with the value 2.
+    What value is left at position 0 after the program halts?
+    '''
+
+    with open(file_input) as data:
+        for line in data:
+            # list comprehension to turn all strings in list to ints
+            input_values = [int(str_num) for str_num in line.split(',')]
+
+            return self.run_max_signal(phase_settings, input_values, start_signal)
+
+
+def create_amplifiers(phase_setting, program):
+    amplifiers = []
+    for phase in phase_setting:
+        amp = IntcodeProgram(program)
+        amp.run(phase)
+        amplifiers.append(amp)
+    return amplifiers
+
+
+def run_max_signal(phase_settings, program):
+    permuted_phases = permutations(phase_settings)
+    max_signal = 0
+
+    for phases in permuted_phases:
+        amplifiers = create_amplifiers(phases, program)
+        input_signal = 0
+        for amp in amplifiers:
+            input_signal = amp.run(input_signal)
+
+        if input_signal > max_signal:
+            max_signal = input_signal
+
+    return max_signal
+
+
 if __name__ == "__main__":
-    sol = IntcodeProgram()
+    program = [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]
+    sol = IntcodeProgram(program)
+    result = sol.run([8])
+    assert result == 1
+    sol = IntcodeProgram(program)
+    result = sol.run([9])
+    assert result == 0
+    sol = IntcodeProgram(program)
+    result = sol.run([7])
+    assert result == 0
 
-    # part 1
+    program = [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]
+    sol = IntcodeProgram(program)
+    result = sol.run([7])
+    assert result == 1
+    sol = IntcodeProgram(program)
+    result = sol.run([8])
+    assert result == 0
+    sol = IntcodeProgram(program)
+    result = sol.run([9])
+    assert result == 0
 
-    input_codes = [3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0]
-    result = sol.run_max_signal([0, 1, 2, 3, 4], input_codes, 0)
+    program = [3, 3, 1108, -1, 8, 3, 4, 3, 99]
+    sol = IntcodeProgram(program)
+    result = sol.run([7])
+    assert result == 0
+    sol = IntcodeProgram(program)
+    result = sol.run([8])
+    assert result == 1
+    sol = IntcodeProgram(program)
+    result = sol.run([9])
+    assert result == 0
+
+    program = [3, 3, 1107, -1, 8, 3, 4, 3, 99]
+    sol = IntcodeProgram(program)
+    result = sol.run([7])
+    assert result == 1
+    sol = IntcodeProgram(program)
+    result = sol.run([8])
+    assert result == 0
+    sol = IntcodeProgram(program)
+    result = sol.run([9])
+    assert result == 0
+
+    program = [3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9]
+    sol = IntcodeProgram(program)
+    result = sol.run([0])
+    assert result == 0
+    sol = IntcodeProgram(program)
+    result = sol.run([1])
+    assert result == 1
+
+    program = [3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1]
+    sol = IntcodeProgram(program)
+    result = sol.run([0])
+    assert result == 0
+    sol = IntcodeProgram(program)
+    result = sol.run([1])
+    assert result == 1
+
+    program = [3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+               1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+               999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99]
+    sol = IntcodeProgram(program)
+    result = sol.run([7])
+    assert result == 999
+    sol = IntcodeProgram(program)
+    result = sol.run([8])
+    assert result == 1000
+    sol = IntcodeProgram(program)
+    result = sol.run([9])
+    assert result == 1001
+
+    # ===============================
+
+    program = [3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0]
+    sol = IntcodeProgram(program)
+    result = sol.run([4, 0])
+    sol = IntcodeProgram(program)
+    result = sol.run([3, result])
+    sol = IntcodeProgram(program)
+    result = sol.run([2, result])
+    sol = IntcodeProgram(program)
+    result = sol.run([1, result])
+    sol = IntcodeProgram(program)
+    result = sol.run([0, result])
     assert result == 43210
-
-    input_codes = [3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23,
-                   101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0]
-    result = sol.run_max_signal([0, 1, 2, 3, 4], input_codes, 0)
-    assert result == 54321
-
-    input_codes = [3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33,
-                   1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0]
-    result = sol.run_max_signal([0, 1, 2, 3, 4], input_codes, 0)
-    assert result == 65210
-
-    result = sol.run_intcode_max_signal([0, 1, 2, 3, 4], 0, "input.txt")
-    assert result == 17406
-
-    # part 2
-
-    input_codes = [3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26,
-                   27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5]
-    result = sol.run_max_signal([5, 6, 7, 8, 9], input_codes, 0)
     print(result)
 
+    program = [3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33,
+               1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0]
+    sol = IntcodeProgram(program)
+    result = sol.run([1, 0])
+    sol = IntcodeProgram(program)
+    result = sol.run([0, result])
+    sol = IntcodeProgram(program)
+    result = sol.run([4, result])
+    sol = IntcodeProgram(program)
+    result = sol.run([3, result])
+    sol = IntcodeProgram(program)
+    result = sol.run([2, result])
+    assert result == 65210
+    print(result)
 
+    program = [3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23,
+               101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0]
+    sol = IntcodeProgram(program)
+    result = sol.run([0, 0])
+    sol = IntcodeProgram(program)
+    result = sol.run([1, result])
+    sol = IntcodeProgram(program)
+    result = sol.run([2, result])
+    sol = IntcodeProgram(program)
+    result = sol.run([3, result])
+    sol = IntcodeProgram(program)
+    result = sol.run([4, result])
+    assert result == 54321
+    print(result)
+
+    # sol = IntcodeProgram(program)
+    # sol.run(3)
+    # output = sol.run(output)
+    #
+    # sol = IntcodeProgram(program)
+    # sol.run(2)
+    # output = sol.run(output)
+    #
+    # sol = IntcodeProgram(program)
+    # sol.run(1)
+    # output = sol.run(output)
+    #
+    # sol = IntcodeProgram(program)
+    # sol.run(0)
+    # output = sol.run(output)
+
+    # assert output == 43210
+    #
+    # input_codes = [3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23,
+    #                101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0]
+    # result = sol.run_max_signal([0, 1, 2, 3, 4], input_codes, 0)
+    # assert result == 54321
+    #
+    # input_codes = [3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33,
+    #                1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0]
+    # result = sol.run_max_signal([0, 1, 2, 3, 4], input_codes, 0)
+    # assert result == 65210
+    #
+    # result = sol.run_intcode_max_signal([0, 1, 2, 3, 4], 0, "input.txt")
+    # assert result == 17406
+    #
+    # # part 2
+    #
+    # input_codes = [3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26,
+    #                27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5]
+    # result = sol.run_max_signal([5, 6, 7, 8, 9], input_codes, 0)
+    # print(result)
