7a20b7c79ebd7bd46c47a6ae1bee970aedd3861e
Ethan Wessel <ejwessel@gmail.com>
2019-12-15 00:47:17 -0500

    setup for second problem

--- Day_8/DecodeSpaceImage.py
+++ Day_8/DecodeSpaceImage.py
@@ -39,6 +39,7 @@ def getSmallestZeroFreq(images):
 
 if __name__ == "__main__":
 
+    #Part 1
     output = parseImage(123456789012, 3, 2)
     image1_freq = getNumFreq(output[0])
     image2_freq = getNumFreq(output[1])
@@ -57,3 +58,42 @@ if __name__ == "__main__":
     assert freq == {'1': 15, '2': 130, '0': 5}
     product = freq['1'] * freq['2']
     assert product == 1950
+
+    #Part 2
+
+    # image rules
+    # 0 is black
+    # 1 is white
+    # 2 is transparent
+    # ordering of image has precedence
+    # 0 + 1 + 2 = 0
+    # 2 + 2 + 0 + 1 = 0
+
+    # 0 + 1 + 2 + 0 = 0
+    # 2 + 1 + 2 + 0 = 1
+    # 2 + 2 + 1 + 0 = 1
+    # 2 + 2 + 2 + 0 = 0
+
+    # seems the rule is to find the first instance of a 0 or 1 when going down layers
+
+    # inspect every pixel
+    final_image = []
+    image_layers = parseImageFile("input.txt", 25, 6)
+    print(image_layers)
+    row_size = len(image_layers[0])
+    col_size = len(image_layers[0][0])
+    for row in row_size:
+        for col in col_size:
+            best_pixel = 3
+            for layer in len(image_layers):
+
+                # go through the layers
+                # get first number smaller than 2
+                # if best pixel has been identified break out of for loop
+                if best_pixel is 3:
+                    best_pixel = image_layers[row][col]
+
+
+
+
+
