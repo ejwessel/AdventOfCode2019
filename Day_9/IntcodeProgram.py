292c17bdcd01fa8791e265f14cb4069a165fa3a3
Ethan Wessel <ethan.wessel@fluidity.io>
2019-12-17 17:20:08 -0500

    test

--- Day_9/IntcodeProgram.py
+++ Day_9/IntcodeProgram.py
@@ -485,6 +485,7 @@ if __name__ == "__main__":
     program = [109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99]
     sol = IntcodeProgram(program)
     output = sol.run(None)
+    assert output == [109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99]
 
     program = [1102, 34915192, 34915192, 7, 4, 7, 99, 0]
     sol = IntcodeProgram(program)
